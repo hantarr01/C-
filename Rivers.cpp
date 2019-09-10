cab5d2ba0db760a08ddc8a75e21ea71e6fea7a7f
hantarr01 <thunderbolt655@mail.ru>
2019-09-10 10:30:39 +0300

    done

---
+++ Rivers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+	using namespace std;
+		int Seine=776;
+		char Garonne=24;
+		wchar_t Meuse=950;
+		double Loire= 1012.01;
+		float Rhin=1325;
+	
+	int main(){
+		cout<<"Seine "<<Seine<<endl;
+		cout<<Garonne<<endl;
+		cout<<"Meuse "<<Meuse<<endl;
+		cout<<"Loire "<<Loire<<endl;
+		cout<<Rhin<<endl;
+		return 0;
+	}
